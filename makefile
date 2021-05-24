37619412c97b81c54bf0577c0919377fa21ad1a4
EgorKharlamov <EgorKharlamov9338@gmail.com>
2021-05-24 12:56:56 +0700

    neovim lua initial settings and keymaps

---
+++ makefile
@@ -0,0 +1,2 @@
+clean_nvim:
+	rm -rf ~/.config/nvim && rm -Rf ~/.local/share/nvim && rm -R ~/.cache/nvim
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias la="exa -la"
+
 alias youtube-dl='/home/nightmare/.local/bin/youtube-dl'
 alias speedtest='wget -O - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
 alias ytdlLA='youtube-dl -wcxi --max-filesize 30m --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s"'
