ca2ce0c6e74b7f793c5da95ddd9cd7caa1121323
EgorKharlamov <EgorKharlamov9338@gmail.com>
2022-12-04 10:11:56 +0700

    soome lit fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,4 @@
 alias la="exa -la"
-alias idea="~/idea"
 
 alias speedtest='wget -O - https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3'
 alias ytdlLA='youtube-dl -wcxi --max-filesize 30m --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s"'
