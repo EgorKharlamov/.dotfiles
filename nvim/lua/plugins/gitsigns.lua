df55163df9eb2b6ce38711d6e1cb23384e5b0c44
EgorKharlamov <EgorKharlamov9338@gmail.com>
2022-12-04 14:02:29 +0700

    setup gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,8 +1,11 @@
 -- import gitsigns plugin safely
 local setup, gitsigns = pcall(require, "gitsigns")
 if not setup then
-  return
+	return
 end
 
+local options = {
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+}
 -- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup(options)
