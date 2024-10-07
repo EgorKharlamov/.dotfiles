dddee2cae5d32bdaf12d8ade4c2f72f227633920
EgorKharlamov <EgorKharlamov9338@gmail.com>
2024-10-07 10:45:23 +0700

    util aliases pats

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,15 +5,15 @@ alias btrestart="$HOME/Dropbox/.scripts/bluetooth/btrestart"
 
 alias ytdlLA='youtube-dl -wcxi --max-filesize 30m --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s"'
 
-alias instapic="/home/nightmare/.scripts/instagram/pics.sh"
-alias showTempRPi="/home/nightmare/.scripts/RPiScripts/cycle.sh '/home/nightmare/.scripts/RPiScripts/showTempRPi.sh -c'"
-alias rap="/home/nightmare/Dropbox/.scripts/music/streamRap.sh"
+alias instapic="$HOME/.scripts/instagram/pics.sh"
+alias showTempRPi="$HOME/.scripts/RPiScripts/cycle.sh '$HOME/.scripts/RPiScripts/showTempRPi.sh -c'"
+alias rap="$HOME/Dropbox/.scripts/music/streamRap.sh"
 alias showclock="tty-clock -scb"
 alias calendar="calcurse"
 alias updg='sudo apt-get update && sudo apt-get upgrade -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo snap refresh'
 alias pacyun="sudo pacman -Syyu --noconfirm && yun"
 alias speedUp='xset r rate 250 50'
-alias chromedev="google-chrome --disable-web-security --user-data-dir=/home/nightmare/chrome"
+alias chromedev="google-chrome --disable-web-security --user-data-dir=$HOME/chrome"
 alias r="ranger"
 
 alias vueDev="echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p"
