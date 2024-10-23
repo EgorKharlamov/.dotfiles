b3b502047c5642d666825bd0bc2c87dfcfb1ab7f
EgorKharlamov <EgorKharlamov9338@gmail.com>
2024-10-23 23:42:49 +0700

    telescope live_grep hiddens

--- nvim/lua/nightmare/lazy/telescope.lua
+++ nvim/lua/nightmare/lazy/telescope.lua
@@ -41,6 +41,11 @@ return {
         },
 
         pickers = {
+          live_grep = {
+            additional_args = function(opts)
+              return {"--hidden"}
+            end
+          },
           find_files = {
             hidden = true,
             no_ignore = true,
